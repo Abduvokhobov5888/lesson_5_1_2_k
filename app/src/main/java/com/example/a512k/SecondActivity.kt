e0d95a886970ffd08303fe65a35467fa6628f63e
Abbosbek <vip.real.aa@gmail.com>
2022-07-28 15:32:06 +0500

    5_1_2_k

---
+++ app/src/main/java/com/example/a512k/SecondActivity.kt
@@ -0,0 +1,25 @@
+package com.example.a512k
+
+import android.content.Intent
+import android.os.Bundle
+import android.text.Html
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.PagerSnapHelper
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.SnapHelper
+
+class SecondActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+    }
+}
